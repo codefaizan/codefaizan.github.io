515a3767e5254b39cd1b3aada4bf3d4942c6e194
Faizan Ahmad <faizan2002a@gmail.com>
2024-06-01 01:31:22 +0500

    added portfolio section

--- script.js
+++ script.js
@@ -13,3 +13,18 @@ const click = () => {
 }
 
 click();
+
+function openModal(modalId) {
+    document.getElementById(modalId).style.display = "block";
+}
+
+function closeModal(modalId) {
+    document.getElementById(modalId).style.display = "none";
+}
+
+// Close modal if user clicks outside of the modal content
+window.onclick = function(event) {
+    if (event.target.classList.contains('modal')) {
+        event.target.style.display = "none";
+    }
+}
